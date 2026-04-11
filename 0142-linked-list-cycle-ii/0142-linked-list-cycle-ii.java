7a9dfdf192657fd89e586f9fdde095d7c641d8b8
Nandini Sahu <nandinisahu2120@gmail.com>
2026-04-11 09:39:15 +0530

    Time: 0 ms (100%), Space: 46.7 MB (57.21%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,21 +11,27 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // ListNode fast=head;
-        // ListNode slow=head;
-        // while(fast!=null && fast.next!=null){
-        //     fast=fast.next.next;
-        //     slow=slow.next;
-        //     if(fast==slow) return fast;
-        // }
-        HashSet<ListNode> st=new HashSet<>();
-        ListNode temp=head;
-        st.add(temp);
-        while(temp!=null && temp.next!=null){
-            if(st.contains(temp.next)) return temp.next;
-            st.add(temp.next);
-            temp=temp.next;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow) break;
         }
-        return null;
+        if(fast==null || fast.next==null) return null;
+        while(head!=slow){
+            head=head.next;
+            slow=slow.next;
+        }
+        return head;
+        // HashSet<ListNode> st=new HashSet<>();
+        // ListNode temp=head;
+        // st.add(temp);
+        // while(temp!=null && temp.next!=null){
+        //     if(st.contains(temp.next)) return temp.next;
+        //     st.add(temp.next);
+        //     temp=temp.next;
+        // }
+        // return null;
     }
 }
