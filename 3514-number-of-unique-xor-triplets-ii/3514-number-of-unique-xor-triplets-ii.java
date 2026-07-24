e8f1bffdf439db5cfb99308f78df524fb389fbf6
Nandini Sahu <nandinisahu2120@gmail.com>
2026-07-24 06:40:47 +0530

    Time: 252 ms (72.73%), Space: 47.1 MB (49.09%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,21 +1,33 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        HashSet<Integer> st1 = new HashSet<>();
+        // HashSet<Integer> st1 = new HashSet<>();
+        int max = -1;
+        for(int it :nums){
+            max = Math.max(max, it);
+        }
+        int u = 1;
+        while( u <= max) u <<= 1;
+        boolean[] ar1 = new boolean[u];
         int n = nums.length;
         for(int i = 0 ; i < n; i++){
             for(int j = i ; j < n ;j++){
-                st1.add(nums[i] ^ nums[j]);
+                ar1[nums[i] ^ nums[j]] = true;
             }
         }
         // System.out.println(st1);
-        HashSet<Integer> st2 = new HashSet<>();
-        for(int it : st1){
-            for(int i = 0 ; i < n ; i++){
-                st2.add(it ^ nums[i]);
+        boolean[] ar2 = new boolean[2048];
+        // HashSet<Integer> st2 /= new HashSet<>();
+        for(int i = 0 ;i < u ; i++){
+            if(!ar1[i]) continue;
+            for(int j = 0 ; j < n ; j++){
+                ar2[i ^ nums[j]] = true;
             }
         }
+        int cnt = 0; 
+        for(boolean it : ar2) if(it)cnt++;
         // System.out.println(st2);
 
-        return st2.size();
+        // return st2.size();
+        return cnt;
     }
 }
