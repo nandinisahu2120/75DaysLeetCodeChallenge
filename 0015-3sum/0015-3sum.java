132c1a99d30017e50bf86dd3e273d1693caf3f99
Nandini Sahu <nandinisahu2120@gmail.com>
2026-07-26 06:59:38 +0530

    Time: 1384 ms (5.64%), Space: 56.6 MB (95.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,71 +1,25 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> li=new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++)
-        {
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int p=i+1,q=nums.length-1;
-            while(p<q){
-                int sum=nums[i]+nums[p]+nums[q];
-                if(sum>0){
-                    q--;
+        List<List<Integer>> li = new ArrayList<>();
+        HashSet<List<Integer>> st = new HashSet<>();
+        int n = nums.length;
+        for(int i = 0 ;i < n ; i++){
+            HashSet<Integer> hp = new HashSet<>();
+            for(int j = i + 1; j < n ; j++){
+                if(hp.contains( -(nums[i] + nums[j]))){
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(-(nums[i] + nums[j]));
+                    temp.add(nums[j]);
+                    Collections.sort(temp);
+                    st.add(temp);
                 }
-                else if(sum<0)
-                    p++;
-                else
-                    {
-                     List<Integer> ll=new ArrayList<>();
-                     ll.add(nums[i]);   
-                     ll.add(nums[p]);   
-                     ll.add(nums[q]);
-                     li.add(ll);
-                     p++;q--;
-                     while(p<q && nums[p]==nums[p-1]) p++;
-                     while(p<q && nums[q]==nums[q+1]) q--;
-                    }
+                hp.add(nums[j]);
             }
+        }
+        for(List<Integer> it: st){
+            li.add(it);
         }
         return li;
     }
-   
 }
-        
-            // for(int j=i+1;j<nums.length-1;j++)
-            // {
-            //     for(int k=j+1;k<nums.length;k++)
-            // {
-            //      if(i==j||j==k||k==i)
-            //      continue;
-            //      else
-            //      {
-            //             if(nums[i]+nums[j]+nums[k]==0)
-            //                 {
-            //                     li.add(nums[i]);
-            //                     li.add(nums[j]);
-            //                     li.add(nums[k]);
-                               
-            //                 }
-            //      }
-                   
-            // }
-                   
-            // }
-        // List<List<Integer>> triplets=new ArrayList<>();
-        // int l=li.size(),c=0;
-        // for(int i=0;i<l/3;i++)
-        // {
-        //         ArrayList<Integer> temp=new ArrayList<>();
-        //         temp.add(li.get(c));
-        //         temp.add(li.get(c+1));
-        //         temp.add(li.get(c+2));
-        //         c+=3;
-        //          Collections.sort(temp);
-        //         triplets.add(temp);
-        // }
-        // Set<List<Integer>> set = new LinkedHashSet<>(triplets);
-
-        // List<List<Integer>> uniqueList = new ArrayList<>(set);
-        // return uniqueList;
-
-         
